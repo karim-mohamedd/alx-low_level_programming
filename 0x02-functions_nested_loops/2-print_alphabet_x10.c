83106de5a3a2cba961653e569e1b6199f71096c1
karim elwaraky <karimelwaraky50@gmail.com>
2023-07-19 06:49:36 -0700

    c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - using _putchar to print alphabet ten times
+ */
+
+int ch ;
+for (int i=0 ;1<10;i++){
+	for(ch = 'a' ;ch <= 'z'; ch++){
+		_putchar(ch);
+	}
+	_putchar("/n")
+}
