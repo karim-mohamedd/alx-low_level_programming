f4628c63fd31b7359ce898c64b07eac6ea8d17cf
karim elwaraky <karimelwaraky50@gmail.com>
2023-07-19 07:57:21 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - gets the abosoulute value of an integer
+ * @n: the int to check
+ * return: the abs of the integer
+ */
+int _abs(int)
+{
+	if (n>0)
+	{
+		return n;
+	}
+	else 
+		return -n;
+}
