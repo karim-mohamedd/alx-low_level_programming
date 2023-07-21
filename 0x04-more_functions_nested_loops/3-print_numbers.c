9bd2ee3cd9688958319cd09fc52926c27d94d660
karim elwaraky <karimelwaraky50@gmail.com>
2023-07-21 13:28:07 -0700

    updated print function

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,11 +6,11 @@
 */
 void print_numbers(void)
 {
-        int num = 0;
-        
-        do {
-                _putchar(num + 48);
-                num++
-        } while (num >= 0 && num <=9 );
-        _putchar('\n');
+	int num = 0;
+
+	do {
+		_putchar(num + 48);
+		num++;
+	} while (num >= 0 && num <= 9);
+	_putchar('\n');
 }
