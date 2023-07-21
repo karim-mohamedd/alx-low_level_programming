275ae36b802bb562d6e332fd2842a1adc408e0a5
karim elwaraky <karimelwaraky50@gmail.com>
2023-07-21 11:23:28 -0700

    updated

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,14 +2,14 @@
 /**
  * isupper - check if char is upper
  *
- * @c: is an input for alphabet 
+ * @c: is an input for alphabet
  *
  * Return: 1 if uppercase. 0 if not
 */
 int _isupper(int c)
 {
-	if ( c >= 65 && c <= 90 )
-		return(1);
-	else 
-		return(0);
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
 }
