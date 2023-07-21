18223f715381eea274514a5d878c7ff2f85eb9ec
karim elwaraky <karimelwaraky50@gmail.com>
2023-07-21 13:02:26 -0700

    print numbers function

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers from 0 to 9
+ *
+ * Return: Always 0 (Success)
+*/
+void print_numbers(void)
+{
+	for (num = 0 ; num <= 9 ; num++)
+	{
+		_putchar(num + 48);
+	}
+	_putchar('/n');
+}
