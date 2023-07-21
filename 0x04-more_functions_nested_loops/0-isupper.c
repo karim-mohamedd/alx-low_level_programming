fcd6d6a0353f31fad7ec741936a64ceb21a2d0ff
karim elwaraky <karimelwaraky50@gmail.com>
2023-07-21 11:17:59 -0700

    is_upper function

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * isupper - check if char is upper
+ *
+ * @c: is an input for alphabet 
+ *
+ * Return: 1 if uppercase. 0 if not
+*/
+int _isupper(int c)
+{
+	if ( c >= 65 && c <= 90 )
+		return(1);
+	else 
+		return(0);
+}
