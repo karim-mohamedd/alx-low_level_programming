9d9775ed7b3a5020aafcd92cea8d39089d320513
karim elwaraky <karimelwaraky50@gmail.com>
2023-07-28 13:21:21 -0700

    reverse

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * reverse_array - reverse array of integers
+ * @a: array
+ * @n: number of elements of array
+ *
+ * Return: void
+*/
+
+void reverse_array(int *a, int n)
+{
+	int i ;
+	int j ;
+
+	while (a[i] != '\0')
+	{
+		i++;
+	}
+	for (j = i; j <= n; j--)
+	{
+		_putchar(a[j]);
+	}
+}
