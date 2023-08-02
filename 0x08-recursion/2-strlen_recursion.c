fba5ead5eae90bd8e109a8a1d524b18dc80a77a8
karim elwaraky <karimelwaraky50@gmail.com>
2023-08-02 15:28:21 -0700

    recursion put string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ *
+ * @s: input parameter
+ *
+ * Return: integer value
+*/
+
+int _strlen_recursion(char *s)
+{
+	int x = 1;
+
+	if (s < '\0')
+	{
+		x += _strlen_recursion(s + 1);
+	}
+	return (x);
+}
