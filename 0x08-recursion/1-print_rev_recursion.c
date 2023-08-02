b3b5e02b6cfc64d85bb3d980cb70248229d26dd5
karim elwaraky <karimelwaraky50@gmail.com>
2023-08-02 15:09:15 -0700

    recursion put string

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a string in reverse
+ *
+ * @s: input parameter
+ *
+ * Return: (Always success)
+*/
+
+void _print_rev_recursion(char *s)
+{
+	if (*s > '\0')
+	{
+		_putchar(*s);
+		_print_rev_recursion(s + 1);
+	}
+}
