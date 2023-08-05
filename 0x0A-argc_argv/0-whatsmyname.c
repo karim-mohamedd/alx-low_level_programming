ab29b207fb8bf92ee3dc3adc392ff7da0b00c014
karim elwaraky <karimelwaraky50@gmail.com>
2023-08-05 14:52:30 -0700

    argc and argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,8 +8,10 @@
  * Return: Always 0 (Success)
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char const *argv[])
 {
-	printf("%s\n", *argv);
+	(void)argc;
+
+	printf("%s\n", argv[0]);
 	return (0);
 }
