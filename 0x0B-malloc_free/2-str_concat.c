d92289f07786368c15f340bf53760ffac8804431
karim elwaraky <karimelwaraky50@gmail.com>
2023-08-08 11:54:35 -0700

    malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -32,11 +32,11 @@ char *str_concat(char *s1, char *s2)
 	}
 	else
 	{
-		for (i = 0; i < size1 + size2; i++)
+		for (int i = 0; i < size1 + size2; i++)
 			if (i < size1)
 				m[i] = s1[i];
 			else
-				m[i] = s2[[i] - size1];
+				m[i] = s2[i - size1];
 	}
 	return (m);
 }
